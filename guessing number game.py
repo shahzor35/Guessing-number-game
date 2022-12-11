dd2035aba94955466be29ea41c42119da427e94a
shahzor35 <116899234+shahzor35@users.noreply.github.com>
2022-12-11 18:37:53 +0530

    Add files via upload

---
+++ guessing number game.py
@@ -0,0 +1,37 @@
+from random import randint
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+def check_answer(guess, answer, turns):
+  if guess > answer:
+    print("Too hight.")
+    return turns - 1
+  elif guess < answer:
+    print("Too low. ")
+    return turns - 1
+  else:
+    print(f"You got it! The answer was {answer}.")
+def set_difficulty():
+  level = input("Choose a difficulty. Type 'easy' or 'hard' level:").lower()
+  if level == "easy":
+
+    return EASY_LEVEL_TURNS
+  else:
+    return HARD_LEVEL_TURNS
+
+
+def game():
+  print("Welcome to the Guessing Game!")
+  print("Iam thinking of a number between 1 to 100.")
+  answer = randint(1,100)
+  turns = set_difficulty()
+  guess = 0
+  while guess != answer:
+    print(f"You have {turns} attemps remaning to guess the number")
+    guess = int(input("make a guess:"))
+    turns = check_answer(guess, answer, turns)
+    if turns == 0:
+      print("You lose")
+      return
+    elif guess!=answer:
+      print("Guess again.")
+game()
